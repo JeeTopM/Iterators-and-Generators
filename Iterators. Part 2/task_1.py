599c8470e7a5f87d55d1acdfcae159980ef124b1
Лев Романович Панас <jieba2005@list.ru>
2025-07-08 23:36:38 +0300

    start task_1

---
+++ Iterators. Part 2/task_1.py
@@ -0,0 +1,26 @@
+'''
+Функция filterfalse()
+Реализуйте функцию filterfalse() с использованием функции filter(), которая принимает два аргумента:
+predicate — функция-предикат; если имеет значение None, то работает аналогично функции bool()
+iterable — итерируемый объект
+Функция должна работать противоположно функции filter(), то есть возвращать итератор,
+элементами которого являются элементы итерируемого объекта iterable, для которых функция predicate вернула значение False.
+'''
+
+def filterfalse():
+    pass
+
+# 1
+objects = [0, 1, True, False, 17, []]
+print(*filterfalse(None, objects))
+# 0 False []
+
+# 2
+numbers = (1, 2, 3, 4, 5)
+print(*filterfalse(lambda x: x % 2 == 0, numbers))
+# 1 3 5
+
+# 3
+numbers = [1, 2, 3, 4, 5]
+print(*filterfalse(lambda x: x >= 3, numbers))
+# 1 2
