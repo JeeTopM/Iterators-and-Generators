589e7370a09cf491c409e985fbaabfdd37f14835
Лев Романович Панас <jieba2005@list.ru>
2025-07-09 00:22:45 +0300

    task1 done

--- Iterators. Part 2/task_1.py
+++ Iterators. Part 2/task_1.py
@@ -7,8 +7,10 @@ iterable — итерируемый объект
 элементами которого являются элементы итерируемого объекта iterable, для которых функция predicate вернула значение False.
 '''
 
-def filterfalse():
-    pass
+def filterfalse(predicate, iterable):
+    if predicate is None:
+        predicate = bool
+    return filter(lambda x: not predicate(x), iterable)
 
 # 1
 objects = [0, 1, True, False, 17, []]
