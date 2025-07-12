1a3f6d832ae9cd0144e5826ea3d1b5d16a1da1d8
Лев Романович Панас <jieba2005@list.ru>
2025-07-12 18:11:18 +0300

    Старт 1 работы

---
+++ Iterators. Part 3/task_1.py
@@ -0,0 +1,20 @@
+"""
+Дополните приведенный ниже код, чтобы в переменной infinite_love содержался итератор,
+бесконечно генерирующий единственное значение — строку i love beegeek!.
+"""
+
+infinite_love = ____
+
+# 1
+print(next(infinite_love))
+# i love beegeek!
+
+# 2
+print(next(infinite_love))
+print(next(infinite_love))
+print(next(infinite_love))
+"""
+i love beegeek!
+i love beegeek!
+i love beegeek!
+"""
