055c736f1fcec409a2b0fef8e92a3b4bb0540e4b
Лев Романович Панас <jieba2005@list.ru>
2025-07-12 18:24:58 +0300

    Задание 1 сделано

--- Iterators. Part 3/task_1.py
+++ Iterators. Part 3/task_1.py
@@ -3,12 +3,12 @@
 бесконечно генерирующий единственное значение — строку i love beegeek!.
 """
 
-infinite_love = ____
+infinite_love = iter(lambda: "i love beegeek!", None)
 
 # 1
 print(next(infinite_love))
 # i love beegeek!
-
+print() # просто пробел
 # 2
 print(next(infinite_love))
 print(next(infinite_love))
